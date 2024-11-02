88f616db3b7558034935d1ea5f2c90b6740684c3
osieltorres <osieltorresdev@gmail.com>
2024-11-02 16:37:22 -0600

    FEAT: Dep whitenoise statics

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ python3 -m pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput
+
 # Create Vercel-compatible output vercel directory
 mkdir -p .vercel/output/static
 cp -r staticfiles/ .vercel/output/static/
